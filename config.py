d4f292c909a3db67936c164d1214d179e2724dbd
kyabegandu <kyabegandu1@gmail.com>
2024-12-15 17:06:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7592369464:AAEaYQV6T7QCAHZCrOh7OmoJhYH_avQ-Lf0")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@spec890i")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS = [5736732360, 7221087191]
+
+
+
+
+
+
+
+
+  
